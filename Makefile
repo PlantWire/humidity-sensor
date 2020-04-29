afce0c17592bf3382fabc6c3faed3ad16dbff3a8
Carlo <carlo.kirchmeier@hsr.ch>
2020-04-29 15:13:48 +0200

    WIP: Enable time based measurement

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ build_lib_test:
 build_prod: build_lib_prod
 	cd exe/ && make build_prod
 
-build_lib_prod:
+build_lib_prod: build_spwl
 	cd lib/ && make build_lib_prod
 
 upload_prod: 
@@ -27,3 +27,12 @@ prepare:
 
 lint:
 	cpplint --filter=-legal/copyright --extensions=c,cc,h,hpp,c++,h++,hh,cu,cpp,hxx,cxx,cuh,ino --recursive --exclude=test/cute --exclude=.metadata .
+
+build_spwl:
+	mkdir -p spwl/build/spwl
+	cp spwl_library.properties spwl/build/spwl/library.properties
+	cp spwl/lib/include/crc16.h spwl/build/spwl
+	cp spwl/lib/include/SPWL.h spwl/build/spwl
+	cp spwl/lib/src/SPWL.cpp spwl/build/spwl
+	sed -i 's/..\/include\/SPWL.h/SPWL.h/' spwl/build/spwl/SPWL.cpp
+	sed -i 's/..\/include\/crc16.h/crc16.h/' spwl/build/spwl/SPWL.cpp
--- exe/src/pwire-sensor.ino
+++ exe/src/pwire-sensor.ino
@@ -1,29 +1,55 @@
-#include <Arduino.h>
-#include <LoRa_E32.h>
-#include <pwire-sensor-lib.h>
+#include "Arduino.h"
+#include "LoRa_E32.h"
+#include "SPWL.h"
+#include <sstream>
 
+// ToDo(ckirchme): D2 or D4
+// ToDo(ckirchme): LoRa_E32 e32ttl100(D2, D3, D5, D7, D6);
 LoRa_E32 e32ttl100(D2, D3);
+constexpr int fiveMinutes = 300000;
+
+int raw= 0;
+int Vin= 5;
+float Vout= 0;
+float R1= 1000;
+float R2= 0;
+float buffer= 0;
 
 void setup() {
   Serial.begin(9600);
   delay(500);
+  pinMode(D7, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(A0, INPUT);
+  digitalWrite(D7, 0);
+  digitalWrite(D6, 0);
   e32ttl100.begin();
-  Serial.println("Hi, I'm going to send message!");
-  ResponseStatus rs = e32ttl100.sendMessage("Hello, world?");
-  Serial.println(rs.getResponseDescription());
+  measure();
 }
 
+ 
+
 void loop() {
-  if (e32ttl100.available() > 1) {
-    ResponseContainer rc = e32ttl100.receiveMessage();
-    if (rc.status.code != 1) {
-      rc.status.getResponseDescription();
-    } else {
-      Serial.println(rc.data);
+  delay(fiveMinutes);
+  measure();
+}
+
+void measure(){
+  raw = analogRead(A0);
+  if(raw)
+  {
+    buffer= raw * Vin;
+    Vout= (buffer)/1024.0;
+    buffer= (Vin/Vout) -1;
+    R2= R1 * buffer;
+
+    std::ostringstream ss;
+    ss << R2;
+    std::string s(ss.str());
+
+    std::pair<SPWLPacket, bool> result = SPWLPacket::encapsulateData(std::vector<unsigned char>{s.cbegin(), s.cend()});
+    if(result.second) {
+      e32ttl100.sendMessage(result.first.rawData().data(), result.first.rawDataSize());
     }
-  }
-  if (Serial.available()) {
-    String input = Serial.readString();
-    e32ttl100.sendMessage(input);
   }
 }
