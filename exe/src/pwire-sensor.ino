4d5a91ebfe10ce029e2663d2ca15edbf8424ae12
Carlo <carlo.kirchmeier@hsr.ch>
2020-05-04 12:47:51 +0200

    Format measurement with correct json

--- exe/src/pwire-sensor.ino
+++ exe/src/pwire-sensor.ino
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 #include "LoRa_E32.h"
 #include "SPWL.h"
-#include <pwire-sensor-lib.h>
+#include <pwire-sensor-lib.h>  // ToDO(ckirchme): linter error with "
 #include <sstream>
 
 // ToDo(ckirchme): D2 or D4
@@ -31,10 +31,21 @@ void measure() {
 
   std::ostringstream ss;
   ss << measurement;
-  std::string s(ss.str());
+  std::string value(ss.str());
+
+  std::string message {
+      "{"
+        "\"Type\": \"HumidityMeasurementResponse\","
+        "\"Target\": \"fe2c15fc-85d2-4691-be70-f4adb326a334\","
+        "\"Sender\": \"575ee151-23ee-4c8b-8c09-1a5c010d516f\","
+        "\"Content\": {"
+          "\"Value\":" + value +
+        "}"
+      "}"};
 
   std::pair<SPWLPacket, bool> result = SPWLPacket::
-      encapsulateData(std::vector<unsigned char>{s.cbegin(), s.cend()});
+      encapsulateData(std::vector<unsigned char>{message.cbegin(),
+                                                 message.cend()});
   if (result.second) {
     e32ttl100.sendMessage(result.first.rawData().data(),
         result.first.rawDataSize());
