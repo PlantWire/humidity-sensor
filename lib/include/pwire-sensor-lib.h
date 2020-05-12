0a88a376caa0c10f94c532ee65991cbc6ec5cf41
Carlo <carlo.kirchmeier@hsr.ch>
2020-05-12 19:06:29 +0200

    WIP: Added code for basic receive

--- lib/include/pwire-sensor-lib.h
+++ lib/include/pwire-sensor-lib.h
@@ -2,7 +2,14 @@
 #define LIB_INCLUDE_PWIRE_SENSOR_LIB_H_
 
 #include <iosfwd>
+#include <cstdint>
+#include "../../spwl/lib/include/SPWL.h"
+
 namespace pwireSensorLib {
-  int calculateMeasurement(int input);
+  int calculateMeasurement(const int input);
+  bool checkPreamble(const int count, const uint8_t value);
+  SPWLHeader getHeader(const uint8_t * data);
+  SPWLPacket::PacketContainer getPacket(const SPWLHeader header,
+      const uint8_t * dataWithChecksum);
 }
 #endif /* LIB_INCLUDE_PWIRE_SENSOR_LIB_H_ */
--- test/src/pwire-sensor-test.cpp
+++ test/src/pwire-sensor-test.cpp
@@ -6,6 +6,7 @@
 #include "../cute/xml_listener.h"
 
 #include "../../lib/include/pwire-sensor-lib.h"
+#include "../../spwl/lib/include/SPWL.h"
 
 void maxMeasurementTest() {
   int result = pwireSensorLib::calculateMeasurement(1023);
@@ -27,12 +28,65 @@ void middleMeasurementTest() {
   ASSERT_EQUALM("Middle measurement not correct!", 80, result);
 }
 
+void preambleCheckTest() {
+  for (int i = 0; i < SPWLPacket::PREAMBLESIZE; i++) {
+    ASSERT_EQUALM("Preamble check not correct!",
+        pwireSensorLib::checkPreamble(i, SPWLPacket::PREAMBLE[i]), true);
+  }
+}
+
+void getHeaderTest() {
+  SPWLHeader input{};
+  input.dataSize = 17;
+  input.senderAddress = 1;
+  input.channel = 23;
+  input.last = true;
+
+  SPWLPacket::HeaderContainer rawHeader =
+      SPWLPacket::getRawFromHeader(input);
+  SPWLHeader result =
+      pwireSensorLib::getHeader(static_cast<uint8_t *>(rawHeader.data()));
+
+  ASSERT_EQUALM("DataSize not correct!", 17, result.dataSize);
+  ASSERT_EQUALM("SenderAddress not correct!", 1, result.senderAddress);
+  ASSERT_EQUALM("Channel not correct!", 23, result.channel);
+  ASSERT_EQUALM("Last value not correct!", true, result.last);
+}
+
+void getPacketTest() {
+  SPWLPacket::DataContainer input {'H', 'e', 'l', 'l', 'o'};
+  std::pair<SPWLPacket, bool> res = SPWLPacket::encapsulateData(input);
+  ASSERTM("Result is missing.", res.second);
+  SPWLPacket::PacketContainer raw = res.first.rawData();
+  SPWLPacket::HeaderContainer rawHeader{};
+  std::copy(raw.cbegin() + SPWLPacket::PREAMBLESIZE,
+      raw.cbegin() + SPWLPacket::PREAMBLESIZE + SPWLPacket::HEADERSIZE,
+      rawHeader.begin());
+  SPWLHeader header = SPWLPacket::getHeaderFromRaw(rawHeader);
+  ASSERT_EQUALM("DataSize not correct!", 5, header.dataSize);
+  SPWLPacket::DataContainer data{};
+  std::copy(raw.cbegin() + SPWLPacket::PREAMBLESIZE + SPWLPacket::HEADERSIZE,
+      raw.cbegin() + SPWLPacket::PREAMBLESIZE + SPWLPacket::HEADERSIZE
+      + header.dataSize + SPWLPacket::CHECKSUMSIZE,
+      std::back_inserter(data));
+
+  // The test itself
+  SPWLPacket::PacketContainer result =
+      pwireSensorLib::getPacket(header, data.data());
+  res = SPWLPacket::encapsulatePacket(result);
+  ASSERTM("Result is missing.", res.second);
+  ASSERT_EQUALM("Data not correct!", input, res.first.getData());
+}
+
 bool runAllTests(int argc, char const *argv[]) {
   cute::suite s;
   s.push_back(CUTE(maxMeasurementTest));
   s.push_back(CUTE(minMeasurementTest));
   s.push_back(CUTE(almostMinMeasurementTest));
   s.push_back(CUTE(middleMeasurementTest));
+  s.push_back(CUTE(preambleCheckTest));
+  s.push_back(CUTE(getHeaderTest));
+  s.push_back(CUTE(getPacketTest));
   cute::xml_file_opener xmlfile(argc, argv);
   cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
   bool success = cute::makeRunner(lis, argc, argv)(s, "AllTests");
